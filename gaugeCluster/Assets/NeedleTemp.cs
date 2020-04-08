2c3603fe86841b8bb0f6614a3adf715696e0e5bd
chris070121 <chris070121@gmail.com>
2020-04-08 05:47:58 -0700

    added tcp comms to support program

--- gaugeCluster/Assets/NeedleTemp.cs
+++ gaugeCluster/Assets/NeedleTemp.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEditor.UI;
 using UnityEngine.UI;
+using System.Collections;
 using System.Threading;
 
 public class NeedleTemp : MonoBehaviour {
