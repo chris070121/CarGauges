56eb44f79eba202080dae6d220898d6ab9d047b7
chris070121 <chris070121@gmail.com>
2020-04-08 17:54:49 -0700

    forgot to add files

---
+++ gaugeCluster/Assets/RecieveCenterPic.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecieveCenterPic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        texture = new Texture2D(10, 10);
+        listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (client == null || !client.Connected)
+        {
+            Connect();
+
+        }
+
+        if (client.Connected)
+        {
+            ReadStream();
+        }
+
+        if (texture != null)
+        {
+            image.texture = texture;
+        }
+    }
+
+    TcpClient client;
+    TcpListener listener;
+    int port = 9997;
+    public RawImage image;
+    private Texture2D texture;
+
+    private void Connect()
+    {
+      
+        try
+        {
+           
+            client = listener.AcceptTcpClient();
+
+            if (client.Connected)
+            {
+
+                Debug.Log("Client connected");
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+
+    }
+  
+    private void ReadStream()
+    {
+        try {
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[999999];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //---read incoming stream---
+                int bytesRead = nwStream.Read(buffer, 0, buffer.Length);
+                ms.Write(buffer, 0, bytesRead);
+
+                bool a = texture.LoadImage(buffer);
+                Debug.Log("Image loaded = " + a);
+            }
+            nwStream.Close();
+
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+    }
+
+}
---
+++ gaugeCluster/Assets/RecieveMessages.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class RecieveMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(client == null || !client.Connected)
+        {
+            Connect();
+        }
+
+        if(client.Connected)
+        {
+            ReadStream();
+        }
+    }
+
+    TcpClient client;
+    TcpListener listener;
+    int port = 9999;
+
+    private void Connect()
+    {
+        listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        try
+        {
+            client = listener.AcceptTcpClient();
+            if (client.Connected)
+            {
+                Debug.Log("Client connected");
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }        
+       
+    }
+
+    private void ReadStream()
+    {
+        client = listener.AcceptTcpClient();
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        //---read incoming stream---
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+        string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Debug.Log(dataReceived);
+    }
+
+}
